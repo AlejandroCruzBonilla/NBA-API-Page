349f6a7f663a5e8f87e05949a285e8f9a9650313
Alex139139 <67702439+AlejandroCruzBonilla@users.noreply.github.com>
2023-09-07 17:18:37 -0600

    Update nbaApi.ts

--- api/nbaApi.ts
+++ api/nbaApi.ts
@@ -8,7 +8,7 @@ const nbaApi = axios.create({
   baseURL: 'https://api-nba-v1.p.rapidapi.com',
   // baseURL: 'https://pokeapi.co/api/v2',
   headers: {
-    'X-RapidAPI-Key': '58c571c5b0mshaa04cf76f172d2ep1d765ejsn9cad4057ee56',
+    'X-RapidAPI-Key': '...',
     'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
   }
 });
